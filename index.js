ccb019085af08e4e6a790dc3da9ff2d41ee32390
M4riaDuda <eduardatakano2206@gmail.com>
2025-04-29 20:40:47 -0300

    Funcionalidade de captura de dados das mensagens

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const http = require('http');
+const socketIO = require('socket.io');
 
 const app = express();
 
 const server = http.createServer(app);
+
+const io = socketIO(server);
 
 const ejs = require('ejs');
 const path = require('path');
@@ -18,6 +21,33 @@ app.engine('html', ejs.renderFile);
 app.use('/', (req, res) => {
     res.render('index.html')
 });
+
+// Lógica do Socket.io - Envio e propagação de mensagens
+
+// Array que simula o banco de dados
+let messages = [];
+
+// Estrutura de conexão do Socket.io
+io.on('connection', socket => {
+
+    // Teste de conexão
+    console.log('NOVO USUÁRIO CONECTADO: ' + socket.id);
+
+    // Recupera e mantém (exibe) as mensagens entre front e o back
+    socket.emit('previousMessage', messages);
+
+    // Lógica de chat quando uma mensagem é enviada
+    socket.on('sendMessage', data => {
+
+        // Adiciona a mensagem no final do array de mensagens
+        messages.push(data);
+
+        socket.broadcast.emit('receiveMessage', data);
+
+    });
+
+});
+
 
 server.listen(3000, () => {
     console.log('CHAT RODANDO EM - HTTP://localhost:3000')
